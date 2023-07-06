32734105534c5cc448d3639fcaffee61a979a3e3
SKavlashvili <134401628+SKavlashvili@users.noreply.github.com>
2023-07-06 13:21:32 +0400

    Add files via upload

---
+++ SQLConnectionWrapper/NpgSQLConnectionWrapper/UserTools/DbContext.cs
@@ -0,0 +1,49 @@
+﻿using Npgsql;
+
+namespace NpgSQLConnectionWrapper.UserTools
+{
+    public abstract class DbContext : IDisposable
+    {
+        public bool ConnectionEstablished { get; private set; }
+
+        private NpgsqlConnection Connection;
+        public DbContext(string connectionString)
+        {
+            ConnectionEstablished = false;
+            Connection = new NpgsqlConnection(connectionString);
+        }
+
+        public async Task<TRes> RunQueryAsync<TRes>(Func<NpgsqlConnection, Task<TRes>> command)
+        {
+            if (!ConnectionEstablished)
+            {
+                Connection.Open();
+                ConnectionEstablished = true;
+            }
+            TRes res;
+            NpgsqlTransaction transaction = await Connection.BeginTransactionAsync();
+            res = await command(transaction.Connection);
+            await transaction.CommitAsync();
+            return res;
+        }
+
+        public TRes RunQuery<TRes>(Func<NpgsqlConnection, TRes> command)
+        {
+            if (!ConnectionEstablished)
+            {
+                Connection.Open();
+                ConnectionEstablished = true;
+            }
+            TRes res;
+            NpgsqlTransaction transaction = Connection.BeginTransaction();
+            res = command(transaction.Connection);
+            transaction.Commit();
+            return res;
+        }
+
+        public void Dispose()
+        {
+            Connection.Dispose();
+        }
+    }
+}
---
+++ SQLConnectionWrapper/TestWebAPI/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestWebAPI.Contexts;
+using Dapper;
+namespace TestWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private Test2Context _context2;
+        private TestContext _context1;
+        public TestController(Test2Context context2, TestContext context)
+        {
+            _context2 = context2;
+            _context1 = context;
+        }
+        [HttpGet("[action]")]
+        public async Task<IActionResult> Test()
+        {
+            List<string> someData = await _context2.RunQueryAsync<List<string>>(async (con) =>
+            {
+                return (await con.QueryAsync<string>("select * from \"Campaigns\"")).ToList();
+            });
+            List<string> someData2 = await _context1.RunQueryAsync<List<string>>(async (con) =>
+            {
+                return (await con.QueryAsync<string>("select * from \"Campaign\"")).ToList();
+            });
+            return Ok();
+        }
+    }
+}
